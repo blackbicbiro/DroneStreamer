1b6abc15a2770b9e9fb0f530bc09f350dfef93ef
blackbicbiro <blackbicbiro@gmail.com>
2018-02-14 17:31:31 +0000

    updates

--- dronestreamer.py
+++ dronestreamer.py
@@ -2,6 +2,7 @@
 import sys
 import time
 import requests
+import threading
 from omxplayer.player import OMXPlayer
 from pathlib import Path
 from time import sleep
@@ -9,8 +10,19 @@ from time import sleep
 
 STREAM_URI = 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov'
 
-player = OMXPlayer(STREAM_URI)
+player = OMXPlayer (STREAM_URI, args=['--live','-b', '--no-osd'])
+print("hello")
+print("hello")
 
-sleep(8)
 
+
+print (player.is_playing())
+
+sleep(4)
+
+player.pause()
+sleep(4)
+player.play()
+sleep(4)
+print (player.get_source())
 player.quit()
--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ else
 	echo "ssh will be enabled on the next boot"
 fi
 
+
+
 #set up GPU memory allocation to 128MB
 if grep -q "gpu_mem=" $CONFIG_FILE; then
 	sed -i '/gpu_mem/c\gpu_mem=128' $CONFIG_FILE
@@ -21,10 +23,38 @@ else
 	echo "gpu_mem=128" >> $CONFIG_FILE
 	echo "Set GPU memory to 128Mb"
 fi
+
+
+
+
+#set HDMI output to CEA standard
+if grep -q "hdmi_group=" $CONFIG_FILE;then
+	 sed -i '/hdmi_group=/c\hdmi_group=1' $CONFIG_FILE
+else
+	echo "SET HDMI mode to CEA"
+	echo "hdmi_group=1" >> $CONFIG_FILE
+fi
+#Set HDMI output to 1280x720p60
+if grep -q "hdmi_mode=" $CONFIG_FILE;then
+         sed -i '/hdmi_mode=/c\hdmi_mode=4' $CONFIG_FILE
+else
+        echo "SET HDMI output to 1200x720p50"
+        echo "hdmi_mode=4" >> $CONFIG_FILE #set consumer electronics assosiati
+fi
+#hdmi signle boost
+if grep -q "hdmi_group=" $CONFIG_FILE;then
+         sed -i '/config_hdmi_boost=/c\config_hdmi_boost=4' $CONFIG_FILE
+else
+        echo "SET HDMI output to 1200x720p50"
+        echo "config_hdmi_boost=4" >> $CONFIG_FILE #set consumer electronics assosiati
+fi
+
+
+
 #Disable screen blanking
-if grep -1 "consoleblank" $KERNAL_FILE; then
+if grep -q "consoleblank=" $KERNAL_FILE; then
 	echo "edited console blank time to 0"
-	sed -i 's/consoleblank=[0-9]/consoleblank=0/' $KERNAL_FILE
+	sed -E -i 's/consoleblank=[0-9]+/consoleblank=0/' $KERNAL_FILE
 else
 	echo "added consoleblank=0 to" $KERNAL_FILE
 	sed -i 's/$/ consoleblank=0/' $KERNAL_FILE
@@ -33,7 +63,7 @@ fi
 
 
 #install packages
-apt-get update -Y
+apt-get update -y
 apt-get install python3-pip -y
 apt-get install omxplayer -y
 pip3 install requests
