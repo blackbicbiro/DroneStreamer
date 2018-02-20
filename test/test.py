d68308123c2d4bc79057e263e4381efac494d99a
blackbicbiro <blackbicbiro@gmail.com>
2018-02-20 17:51:21 +0000

    updates

--- test/test.py
+++ test/test.py
@@ -1,18 +1,148 @@
 #!/usr/bin/env python3
+import time
+import os
+import sys
 import socket, struct
-
-def get_default_gateway_linux():
-    """Read the default gateway directly from /proc."""
-    with open("/proc/net/route") as fh:
-        for line in fh:
-            fields = line.strip().split()
-            if fields[1] != '00000000' or not int(fields[3], 16) & 2:
-                continue
-
-            return socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
+import signal
+import requests
+import json
+from pathlib import Path
+from time import sleep
 
 
+#1. check connection to GW
+	#loop
+		#wait 1 second
+#2. check api for camera attached
+#3. check switch position
+#4. start player
+#6. update display
+#7. loop
+	#1. check player is still playing
+		# if player fails start from  begining
+	#2. check switch position
+		# stop player start again fro begining
 
 
-if __name__ == '__main__':
-    print(get_default_gateway_linux())
+#STREAM_URI = 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov'
+Stream_Url = 'rtsp://172.25.40.240/video1'
+API_Url = 'https://jsonplaceholder.typicode.com/todos'
+
+
+
+
+
+#Get default gateway from /proc/net/route
+def get_default_gateway():
+	GW = None
+	while GW == None:
+		GW = os.popen("ip route | grep default | awk {'print$3'}").read()
+		GW = GW.rstrip()
+		if len(GW) == 0:
+			GW = None
+			sleep(1)
+	return GW
+
+
+
+#Fetch Json file and status code
+def fetch_Camera_API(IPaddr):
+	URL = "https://"+IPaddr
+	#URL = "https://"+IPaddr+"/api/getVideoStreams"	#build Api URL from IP
+	print(URL)
+	try:
+		r = requests.get(URL)
+		r.raise_for_status()
+	except requests.exceptions.HTTPError as errh:
+		print ("Http Error:",errh)
+		return None
+	except requests.exceptions.ConnectionError as errc:
+		print ("Error Connecting:",errc)
+		return None
+	except requests.exceptions.Timeout as errt:
+		print ("Timeout Error:",errt)
+		return None
+	except requests.exceptions.TooManyRedirects as errrd:
+		print ("to many Redirects:",errrd)
+		return None
+	except requests.exceptions.RequestException as err:
+		print ("OOps: Something Else",err)
+		return None
+	except:
+		print("connection fault unknown")
+		return None
+
+	if r.status_code == 200:
+		r = json.loads(r.text)	#convert json to python values
+	else:
+		r = None
+
+	return r
+	#print("\n",r.status_code)	#print status code only
+
+	#data = json.loads(r.text)
+#	print(len(data))
+#	print(data)
+#	print(data[0]['userId'])  #print spercific data from dictionar
+
+
+
+
+def Api_Object_Count(data):
+	count = len(data)
+	print(count)
+	
+	return count
+
+
+
+
+########################### MAIN ######################################
+def main():
+
+	while True:
+
+		defaultGW = get_default_gateway()
+		#ApiData = fetch_Camera_API('jsonplaceholder.typicode.com/posts')   #test api url
+		ApiData= fetch_Camera_API('api.myjson.com/bins/l6j55') #http://myjson.com/l6j55 jason emulator
+		#print(ApiData)
+		
+		if ApiData == None:
+			print("no api data, check connection and API again")
+			#start again
+		else:
+			print("got API data, assign camera URLs")
+			CamCount = Api_Object_Count(ApiData)	##count number of camears
+			CamInfo={}	#diconatry
+			CamInfo.clear()
+			#fetch API Infomation
+			for x in range(CamCount):		#get Cam Name and Stream
+				CamInfo[x] = {}
+				CamInfo[x]['Name'] = ApiData[x]['camera_name']
+				CamInfo[x]['Stream'] = ApiData[x]['rtsp_link']
+				print(CamInfo[x]['Name'])
+				print(CamInfo[x]['Stream'])
+
+
+
+
+			#print(ApiData[x]['id'])
+			
+			#print(ApiData[x]['camera_name'])
+			#print(ApiData[x]['rtsp_link'])
+
+
+
+#			print("\n", ApiData[0])
+#			print(ApiData[0]['userId'])
+
+
+		break	#kill while loop while testing
+
+
+
+
+if __name__ == "__main__":	#main program loop
+	main()
+
+
