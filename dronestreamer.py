c9fe635ca7c3562593d5a134aad550b5401da567
blackbicbiro <blackbicbiro@gmail.com>
2018-03-13 20:52:42 +0000

    fixed lockup with curl

--- dronestreamer.py
+++ dronestreamer.py
@@ -15,37 +15,38 @@ import Adafruit_CharLCD as LCD
 
 droneRxIP="api.myjson.com"
 #api.myjson.com/bins/m0k5t
-#apiURL="/bins/m0k5t"		#test with 2 working
-apiURL="/bins/18fa25"		#test with 1 working 1 wrong url, siulates missing camera feed
+apiURL="/bins/m0k5t"		#test with 2 working
+#apiURL="/bins/18fa25"		#test with 1 working 1 wrong url, siulates missing camera feed
 
-GPIO.setmode(GPIO.BCM)     #use GPIO numbers
 
+GPIO.setmode(GPIO.BCM)     #use GPIO numbering
 # Raspberry Pi pin configuration: GPIO numbers
-ButtonPin=27
-lcd_rs        = 26  # Note this might need to be changed to 21 for older revision Pi's.
-lcd_en        = 19
-lcd_d4        = 13
-lcd_d5        = 6
-lcd_d6        = 5
-lcd_d7        = 11
-lcd_backlight = 4		##needs connecting to this pin if i want to control LCD  backlight
-
-GPIO.setup(ButtonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+ButtonPin		= 27
+lcd_rs        	= 26
+lcd_en        	= 19
+lcd_d4        	= 13
+lcd_d5        	= 6
+lcd_d6        	= 5
+lcd_d7        	= 11
+lcd_backlight 	= 4		##needs connecting to this pin if i want to control LCD  backlight
 
 # Define LCD column and row size for 16x2 LCD.
-lcd_columns = 16
-lcd_rows    = 2
+lcd_columns 	= 16
+lcd_rows    	= 2
 
 # Initialize the LCD using the pins above.
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-                           lcd_columns, lcd_rows, lcd_backlight)
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-#os.system("sudo fbi -T 1 --noverbose /home/pi/DronerStreamer/splash.png")  #backgroup picture
-
-sleep(2)
+#define button with pull up resister
+GPIO.setup(ButtonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
-#Check API Connection to Drone Fetch Json file and status code
+
+#Start background Logo
+os.system("sudo fbi -T 1 --noverbose /home/pi/DronerStreamer/splash.png")  #backgroup picture
+
+
+##########Check API Connection to Drone Fetch Json file and status code##########
 def fetch_Camera_API(IPaddr, apiSyntax):
 	#URL = "http://"+IPaddr
 	URL = "http://"+IPaddr+apiSyntax	#build Api URL from IP
@@ -54,7 +55,7 @@ def fetch_Camera_API(IPaddr, apiSyntax):
 	while True:
 		print(URL)
 		try:
-			r = requests.get(URL)
+			r = requests.get(URL, timeout=4)
 			r.raise_for_status()
 			if r.status_code == 200:
 				r = json.loads(r.text)	#convert json to python values
@@ -78,7 +79,7 @@ def fetch_Camera_API(IPaddr, apiSyntax):
 
 
 
-
+########## Validates userSelected stream number against camera list ##########
 def Stream_Selection_button(feedList,SelectedStream):
 	noOfCameras = len(feedList)
 	if SelectedStream > noOfCameras-1:
@@ -90,29 +91,31 @@ def Stream_Selection_button(feedList,SelectedStream):
 		return SelectedStream
 
 
+
+########## Checks for an active RTSP session for omxplayer ##########
 def do_CheckCamUrlStatus(cam,streamNo):	#check camera stream is up by checking rtsp descibe
-		streamStatus =os.system("curl -i "+cam[streamNo]['Stream'])
+		#########May need to grep for the workd DESCRIBE to narrow down exit code##########
+		streamStatus = os.system("curl -m 4 -i "+cam[streamNo]['Stream'])
+		print("cURL check output:", streamStatus)
 		if streamStatus == 0:
 			return True
 		else:
 			return False
 
+
+
+
+
 ########################### MAIN ######################################
 def main():
-	
-#	os.system("pkill omxplayer") #make sure OMXplayer isnt running
 	userSelectedStream = 0
 	GPIO.add_event_detect(ButtonPin, GPIO.RISING, bouncetime=500)	#set up button detechtion with debounce
 
 	while True:	##main loop
 		while True:
 
-
 			ApiData = fetch_Camera_API(droneRxIP,apiURL)		#Get Camera list
-
-			if ApiData == None:
-				print("No API data, check connection and API again")
-			else:
+			if len(ApiData) > 0:
 				CamCount = len(ApiData) 		#count number of camera objects in json file
 				print("Camera Count: ",CamCount)	#count number of camears
 				CamInfo={}
@@ -141,21 +144,19 @@ def main():
 			try:
 				if player1.is_playing() == True:
 					if CamInfo[userSelectedStream]['Stream'] != player1.get_source():
-						if(do_CheckCamUrlStatus(CamInfo, userSelectedStream)) == True:
+						if do_CheckCamUrlStatus(CamInfo, userSelectedStream) == True:
 							player1.load(CamInfo[userSelectedStream]['Stream'])
 							lcd.clear()							#print message to LCD
 							lcd.message("Streaming\n")
 							lcd.message(CamInfo[userSelectedStream]['Name'])
 						else:
-							print("stream does not exist")
+							print("stream does not exist123")
 							break
 					else:
 						print("Currently Streaming",player1.get_source())
 
-
-
 			except:
-				if(do_CheckCamUrlStatus(CamInfo, userSelectedStream)) == True:
+				if do_CheckCamUrlStatus(CamInfo, userSelectedStream) == True:
 					player1 = OMXPlayer(CamInfo[userSelectedStream]['Stream'], args=['--live','-b', '--no-osd', '--threshold','0'])
 					lcd.clear()							#print message to LCD
 					lcd.message("Streaming\n")
@@ -164,26 +165,13 @@ def main():
 					print("stream does not exist")
 					break
 
-
-
-
-				#lcd.clear()							#print message to LCD
-				#lcd.message("Streaming\n")
-				#lcd.message(CamInfo[userSelectedStream]['Name'])
-
-
-#			except:
-#				print("player stopped / Video disconnected / stream unavaliable")
-#				lcd.clear()
-#				lcd.message("  Disconnected  \n")
-#				lcd.message(" Player Stopped ")
-#				try:
-#					player1.quit()
-#					break
-#				except:
-#					break
-
 			sleep(.1)
+
+
+
+
+
+
 
 
 
